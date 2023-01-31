b6d47bddadbc3a9045f4331c5e8f38f496f23fc7
Nadzeya <lustra2013@gmail.com>
2023-01-31 14:55:14 +0300

    math_op.py

---
+++ math_op.py
@@ -0,0 +1,23 @@
+
+
+def sum(a, b):
+    return int(a) + int(b)
+
+def sub(a, b):
+    return int(a) - int(b)
+
+def mult(a, b):
+    return int(a) * int(b)
+
+def div_rem(a, b):   
+        return int(a) % int(b)
+    
+def div_all(a, b):
+    return round(int(a) / int(b), 2)
+    
+def div_int(a, b):
+    return int(a) // int(b)
+
+def pow(a, b):
+    return int(a) ** int(b)
+
