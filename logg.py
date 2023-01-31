8c1592e4ae192f7ca43f9b65d799e9fca4b047d9
Nadzeya <lustra2013@gmail.com>
2023-01-31 19:22:15 +0300

    ch mesage

--- logg.py
+++ logg.py
@@ -4,5 +4,4 @@ logging.basicConfig(
     level=logging.DEBUG,
     filename="my_log.log",
     format="%(asctime)s - %(module)s - %(levelname)s - %(funcName)s - %(message)s",
-    datefmt='%d-%m-%Y %H:%M:%S',
-)
+    datefmt='%d-%m-%Y %H:%M:%S',)
